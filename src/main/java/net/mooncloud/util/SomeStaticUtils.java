aeeb25703dd95b4187122d504245cf6f54b3f9e2
yangjd <yangjd@asiainfo.com>
2015-04-10 11:11:49 +0800

    new file: SomeStaticUtils

---
+++ src/main/java/net/mooncloud/util/SomeStaticUtils.java
@@ -0,0 +1,17 @@
+package net.mooncloud.util;
+
+import java.text.SimpleDateFormat;
+
+public class SomeStaticUtils {
+	public final static SimpleDateFormat DATEFORMAT1 = new SimpleDateFormat(
+			"yyyy-MM-dd HH:mm:ss");
+	public final static SimpleDateFormat DATEFORMAT2 = new SimpleDateFormat(
+			"yyyy-MM-dd HH");
+	public final static SimpleDateFormat DATEFORMAT3 = new SimpleDateFormat(
+			"yyyyMMdd");
+	public final static SimpleDateFormat DATEFORMAT4 = new SimpleDateFormat(
+			"yyyy-MM-dd");
+	// public static final Gson gson = new GsonBuilder().disableHtmlEscaping()
+	// .setLongSerializationPolicy(LongSerializationPolicy.STRING)
+	// .create();
+}
