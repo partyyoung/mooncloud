693aebd0e5d9ad1acc6ea9b0748b5e4c91e84a6f
yangjd <yangjd@asiainfo.com>
2015-07-07 10:27:54 +0800

    add gson pom

--- src/main/java/net/mooncloud/util/SomeStaticUtils.java
+++ src/main/java/net/mooncloud/util/SomeStaticUtils.java
@@ -3,6 +3,10 @@ package net.mooncloud.util;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.LongSerializationPolicy;
+
 public class SomeStaticUtils
 {
 	public final static SimpleDateFormat DATEFORMAT1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -12,7 +16,8 @@ public class SomeStaticUtils
 
 	public final static DecimalFormat df = new DecimalFormat("#.00");
 
-	// public static final Gson gson = new GsonBuilder().disableHtmlEscaping()
-	// .setLongSerializationPolicy(LongSerializationPolicy.STRING)
-	// .create();
+	public static final Gson gson = new GsonBuilder().disableHtmlEscaping()
+	 .setLongSerializationPolicy(LongSerializationPolicy.STRING)
+	 .create();
+	public static final Yaml yaml = new Yaml();
 }
